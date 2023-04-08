9dffefe15bdfd23095b79b772d38e1ffe3d90293
ezequiel3925 <51267285+ezequiel3925@users.noreply.github.com>
2023-04-08 16:27:37 -0300

    9

--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import Card from '../Components/Card'
 import { useContext } from 'react'
 import ThemeContext from '../Components/utils/Context'
